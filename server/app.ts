d5d0973b3ab1e39ad996b36fa701bfc2659eacea
Raghad-Mezied <rmezied3@gmail.com>
2022-04-27 18:55:49 +0300

    fix backend eslint && format files

--- server/app.ts
+++ server/app.ts
@@ -1,19 +1,20 @@
-import {join} from 'path';
+import { join } from 'path';
 import 'express-async-errors';
 import dotenv from 'dotenv';
-import express, {Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(compression())
-app.use(cookieParser())
+app.use(compression());
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '..', 'client', 'build')));
