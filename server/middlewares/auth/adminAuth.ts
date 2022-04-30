1b7605f88a70195c277c70fc3a60486a56bdd768
ManarAbed <manarabed333@gmail.com>
2022-04-30 16:12:21 +0300

    eslint updates relates #10

--- server/middlewares/auth/adminAuth.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const adminAuth = (req: Request, res: Response, next: NextFunction) => {
-  const { userInfo } = req;
-  // eslint-disable-next-line no-unused-expressions
-  userInfo.role === 'admin' ? next() : res.status(401).json({ message: 'You are unathorized' });
-};
-
-export default adminAuth;
