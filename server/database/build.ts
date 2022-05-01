98b44939405f1f6b27baceab198e60776deeba4c
abedshamia <ab.shamia96@gmail.com>
2022-05-01 03:43:58 +0300

    Refactor the functions and Close Connection

--- server/database/build.ts
+++ server/database/build.ts
@@ -10,32 +10,54 @@ import Interviewee from './models/interviewee';
 import Interviewer from './models/interviewer';
 import Schedule from './models/schedule';
 
-mongoose.deleteModel('User');
-mongoose.deleteModel('Interviewee');
-mongoose.deleteModel('Interviewer');
-mongoose.deleteModel('Schedule');
-const createFakeData: Function = () => {
-  usersData.user.forEach((user) => {
-    User.create(user);
-  });
-
-  scheduleData.schedule.forEach((schedule) => {
-    Schedule.create(schedule);
-  });
-
-  intervieweesData.interviewee.forEach((interviewee) => {
-    Interviewee.create(interviewee);
-  });
-
-  interviewersData.interviewer.forEach((interviewer) => {
-    Interviewer.create(interviewer);
-  });
-
-  console.log('Fake data created successfully');
+// Delete a collection
+const deleteCollection = async (model: any) => {
+  try {
+    await model.deleteMany({});
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-startDb().then(() => {
-  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-    createFakeData();
+const createFakeData = async () => {
+  try {
+    usersData.user.forEach(async (user: object) => {
+      await User.create(user);
+    });
+
+    scheduleData.schedule.forEach(async (schedule: object) => {
+      await Schedule.create(schedule);
+    });
+
+    intervieweesData.interviewee.forEach(async (interviewee: object) => {
+      await Interviewee.create(interviewee);
+    });
+
+    interviewersData.interviewer.forEach(async (interviewer: object) => {
+      await Interviewer.create(interviewer);
+    });
+
+    console.log('Fake data created successfully');
+  } catch (err: any) {
+    throw new Error(err);
   }
-});
+};
+
+const main = async () => {
+  try {
+    await startDb();
+    await deleteCollection(User);
+    await deleteCollection(Interviewee);
+    await deleteCollection(Interviewer);
+    await deleteCollection(Schedule);
+    await createFakeData();
+
+    setTimeout(() => {
+      mongoose.connection.close();
+    }, 1500);
+  } catch (err: any) {
+    throw new Error(err);
+  }
+};
+
+main();
