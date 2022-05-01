6e222d354256ec837f449c5436a4a540c907a511
ManarAbed <manarabed333@gmail.com>
2022-05-01 15:39:01 +0300

    create checkAuth controller relates #12

--- server/controllers/auth/checkAuth.ts
+++ server/controllers/auth/checkAuth.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 
 const checkAuth = (req: any, res: Response) => {
   const { userInfo } = req;
-  res.status(200).json({ data: userInfo });
+  res.json({ data: userInfo });
 };
 
 export default checkAuth;
