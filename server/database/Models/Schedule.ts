066be914bd23cab1939eb58aa9c250b79254709e
Raghad-Mezied <rmezied3@gmail.com>
2022-05-01 15:02:13 +0300

    edit models files name

---
+++ server/database/Models/Schedule.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+
+const schedule = new Schema({
+  languages: {
+    type: String,
+    required: false,
+    enum: ['JS', 'PHP', 'C++', 'C#', 'RUBY', 'PYTHON', 'JAVA', 'C', 'GO'],
+  },
+  specialization: {
+    type: String,
+    required: false,
+    enum: ['FRONTEND', 'BACKEND', 'DEVOPS', 'SECURITY', 'DATA STRUCTURE', 'FULL STACK'],
+  },
+  questionCategory: {
+    type: String,
+    required: false,
+    enum: ['Technical', 'Analytical', 'Algorithms', 'System Design'],
+  },
+  available: {
+    type: [
+      {
+        interviewerId: {
+          $ref: 'Interviewer',
+          type: String,
+          required: true,
+        },
+        date: {
+          type: Date,
+          required: true,
+        },
+        time: {
+          type: Array,
+          required: true,
+        },
+      },
+    ],
+    required: false,
+  },
+
+});
+
+const Schedule = model('Schedule', schedule);
+
+export default Schedule;
