c9cb37b02c8ac7cef25858e08bf198cece7bd835
ManarAbed <manarabed333@gmail.com>
2022-05-08 16:25:21 +0300

    fix router endpoint relates #12

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
 import { login, checkAuth } from '../controllers';
-// import { userAuth } from '../middlewares/auth';
+import { userAuth } from '../middlewares/auth';
 
 const router: Router = express.Router();
 
 router.post('/login', login);
-router.get('/checkAuth', checkAuth);
+router.get('/checkAuth', userAuth, checkAuth);
 
 export default router;
