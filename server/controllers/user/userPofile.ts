2ee26b63517ecb6713e01b6201cfa3920953cdc7
ManarAbed <manarabed333@gmail.com>
2022-05-09 13:24:32 +0300

    update response obj on controller relates #34

--- server/controllers/user/userPofile.ts
+++ server/controllers/user/userPofile.ts
@@ -4,8 +4,15 @@ import User from '../../database/Models/User';
 
 const getUserByID = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const userInfo = await User.find({ _id: new ObjectId(id) });
-  res.status(200).json({ data: userInfo });
+
+  const user : any = await User.findOne({ _id: new ObjectId(id) });
+  res.status(200).json({
+    data: {
+      name: user.name,
+      bio: user.bio,
+      profile_picture: user.profile_picture,
+    },
+  });
 };
 
 export default getUserByID;
