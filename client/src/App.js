d2f3bff5da1eae3fe21f18215960e2995badbb03
abedshamia <ab.shamia96@gmail.com>
2022-05-09 09:39:35 +0300

    User checkUser in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkUser } from './redux/features/auth/authSlice';
 
 function App() {
-  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state) => state.auth);
+  React.useEffect(() => {
+    dispatch(checkUser());
+  }, [user]);
 
-  return <div>{user ? 'Logged In' : 'Not Logged In'}</div>;
+  return (
+    <div className="App">
+      {loading ? 'Loading...' : (
+        <div>
+          {user ? (
+            <div>
+              <h1>
+                Welcome,
+                {' '}
+                {user}
+              </h1>
+            </div>
+          ) : (
+            <div>
+              <h1>Please login</h1>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -2,7 +2,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from './authService';
 
-const checkUser = createAsyncThunk('auth/checkUser', async () => authService.checkUser());
+export const checkUser = createAsyncThunk('auth/checkUser', async () => authService.checkUser());
 // Create the initial state for the auth feature
 const initialState = {
   isAuthenticated: false,
