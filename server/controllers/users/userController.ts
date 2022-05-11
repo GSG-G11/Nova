fc8d29820f5ac94c381d610ee7b5afcf8b45ce6d
abedshamia <ab.shamia96@gmail.com>
2022-05-11 12:12:34 +0300

    Removed unnecessary validations in userController, and return all reviews if saved not there

--- server/controllers/users/userController.ts
+++ server/controllers/users/userController.ts
@@ -1,55 +1,69 @@
 import { Response } from 'express';
 import Interviewee from '../../database/Models/Interviewee';
-import User from '../../database/Models/User';
-import { CustomError, RequestType } from '../../utils';
+import { RequestType } from '../../utils';
 import validateQuery from '../../utils/validation/queryValidation';
 
 const getAllReviews = async (req: RequestType, res: Response) => {
-  const { page = '1', saved = 'false' } = req.query;
-
-  // Convert the incoming saved string into a boolean
-  const savedBoolean : Boolean = JSON.parse(saved.toString());
+  const { page = '1' } = req.query;
 
   await validateQuery(req.query);
-  const id = req.userInfo?.id;
-  const user : object | null = await User.findById(id);
+  // Convert the incoming saved string into a boolean
+  const savedBoolean : any = req.query.saved ? JSON.parse(req.query.saved.toString())
+    : req.query.saved;
 
-  if (!user) {
-    throw new CustomError('User not found', 404);
-  }
-  if (!Number(page)) {
-    throw new CustomError('Invalid Page', 400);
-  }
-
-  if (typeof savedBoolean !== 'boolean') {
-    throw new CustomError('Invalid saved', 400);
-  }
+  // const id = req.userInfo?.id;
 
   // filter reviews based on the saved parameter and paginate the results
-  const filteredReviews = await Interviewee.aggregate([{
-    $project: {
-      _id: 0,
-      'interviews.review': 1,
-    },
-  },
-  {
-    $unwind: '$interviews',
-  },
-  {
-    $match: {
-      'interviews.review.saved': savedBoolean,
-    },
-  },
-  {
-    $group: {
-      _id: '$interviews.review._id',
-      review: {
-        $first: '$interviews.review',
+  // TODO: Replace the static userId with the logged in user's id when create review is implemented
+  let filteredReviews : any | undefined;
+  if (savedBoolean === true || savedBoolean === false) {
+    filteredReviews = await Interviewee.aggregate([{
+      $project: {
+        _id: 0,
+        userId: 1,
+        'interviews.review': 1,
       },
     },
-  },
-  ]).skip((Number(page) - 1) * 3).limit(3);
 
+    {
+      $unwind: '$interviews',
+    },
+    {
+      $match: {
+        userId: '4',
+        'interviews.review.saved': savedBoolean,
+      },
+    },
+    {
+      $group: {
+        _id: '$interviews.review._id',
+        review: {
+          $first: '$interviews.review',
+        },
+      },
+    },
+    ]).skip((Number(page) - 1) * 3).limit(3);
+  }
+
+  if (savedBoolean === undefined) {
+    filteredReviews = await Interviewee.aggregate([{
+      $project: {
+        _id: 0,
+        userId: 1,
+        'interviews.review': 1,
+      },
+    },
+    {
+      $match: {
+        userId: '4',
+      },
+    },
+    {
+      $unwind: '$interviews',
+    },
+
+    ]).skip((Number(page) - 1) * 3).limit(3);
+  }
   return res.json({
     message: 'Reviews found',
     length: filteredReviews.length,
--- server/utils/validation/queryValidation.ts
+++ server/utils/validation/queryValidation.ts
@@ -3,7 +3,7 @@ import Joi, { ValidationResult } from 'joi';
 const validateQuery = async (query: any) => {
   const querySchema = Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    saved: Joi.boolean().default(false),
+    saved: Joi.boolean(),
   });
 
   const result: ValidationResult = await querySchema.validateAsync(query);
