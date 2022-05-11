d39a2972c6a082aa13483867f8fc66db5371a6f0
abedshamia <ab.shamia96@gmail.com>
2022-05-11 10:27:38 +0300

    Get reviews from the interview object

--- server/controllers/users/userController.ts
+++ server/controllers/users/userController.ts
@@ -7,9 +7,13 @@ import validateQuery from '../../utils/validation/queryValidation';
 const getAllReviews = async (req: RequestType, res: Response) => {
   const { page = '1', saved = 'false' } = req.query;
 
+  // Convert the incoming saved string into a boolean
+  const savedBoolean : Boolean = JSON.parse(saved.toString());
+
   await validateQuery(req.query);
   const id = req.userInfo?.id;
   const user : object | null = await User.findById(id);
+
   if (!user) {
     throw new CustomError('User not found', 404);
   }
@@ -17,38 +21,40 @@ const getAllReviews = async (req: RequestType, res: Response) => {
     throw new CustomError('Invalid Page', 400);
   }
 
-  if (saved !== 'true' && saved !== 'false') {
-    throw new CustomError('Invalid Input', 400);
-  }
-
-  // TODO: replace the userId string with the userId from the user object
-  const userInterviews = await Interviewee.find({ userId: '4' });
-
-  if (userInterviews.length === 0) {
-    return res.json({
-      message: 'No interviews found',
-    });
+  if (typeof savedBoolean !== 'boolean') {
+    throw new CustomError('Invalid saved', 400);
   }
 
   // filter reviews based on the saved parameter and paginate the results
   const filteredReviews = await Interviewee.aggregate([{
+    $project: {
+      _id: 0,
+      'interviews.review': 1,
+    },
+  },
+  {
     $unwind: '$interviews',
   },
   {
     $match: {
-      'interviews.review.saved': saved === 'true',
+      'interviews.review.saved': savedBoolean,
     },
-  }]).skip((Number(page) - 1) * 3).limit(3);
+  },
+  {
+    $group: {
+      _id: '$interviews.review._id',
+      review: {
+        $first: '$interviews.review',
+      },
+    },
+  },
+  ]).skip((Number(page) - 1) * 3).limit(3);
 
-  // Get the reviews of the user
-  const reviews = filteredReviews.map((interviewee) => ({
-    review: interviewee.interviews.review,
-  }));
   return res.json({
     message: 'Reviews found',
-    length: reviews.length,
+    length: filteredReviews.length,
     page: Number(page),
-    data: reviews,
+    data: filteredReviews,
   });
 };
 
