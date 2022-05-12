0221b43800406f696c1f5f2c7ecbb107eb778382
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-12 14:35:39 +0300

    throw error if no data updated

--- server/controllers/users/updateInfo.ts
+++ server/controllers/users/updateInfo.ts
@@ -8,10 +8,6 @@ import {
 const updateInfo = async (req: RequestType, res: Response) => {
   const { userInfo } = req;
 
-  if (!userInfo) {
-    throw new CustomError('User not found', 404);
-  }
-
   const {
     profile_picture, cv, bio, level,
   }: updateInfoInterface = req.body;
@@ -20,14 +16,16 @@ const updateInfo = async (req: RequestType, res: Response) => {
     profile_picture, cv, bio, level,
   });
 
-  const user = await User.updateOne({ _id: userInfo.id }, {
+  const user = await User.updateOne({ _id: userInfo?.id }, {
     $set: {
       profile_picture, cv, bio, level,
     },
   });
 
-  return (user.modifiedCount > 0) ? res.json({ message: 'User updated successfully' })
-    : res.status(200).json({ message: 'No data updated' });
+  if (user.modifiedCount > 0) {
+    return res.json({ message: 'User updated successfully' });
+  }
+  throw new CustomError('Update failed', 500);
 };
 
 export default updateInfo;
