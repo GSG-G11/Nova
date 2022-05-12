02408c16ae60156b96ac6b74805945ef3afba7d4
ManarAbed <manarabed333@gmail.com>
2022-05-12 13:32:38 +0300

    send cancal intrview email relates #41

--- server/controllers/intreview/cancalIntreview.ts
+++ server/controllers/intreview/cancalIntreview.ts
@@ -1,13 +1,33 @@
+/* eslint-disable space-before-blocks */
 import { Response } from 'express';
-import { RequestType } from '../../utils';
+import { RequestType, mailSender } from '../../utils';
 import Interviewee from '../../database/Models/Interviewee';
+import Interviewer from '../../database/Models/Interviewer';
+import User from '../../database/Models/User';
 
 const cancalInterview = async (req: RequestType, res: Response) => {
   // use vaildation for params after marge
   const { interviewId } : any = req.params;
+  const { userInfo } = req;
 
-  // find the intreview object by interviewId
-  const interview = await Interviewee.findOne({ 'interviews._id': interviewId }, { interviews: 1, _id: 0 });
+  const user = await User.findOne({ _id: userInfo?.id }, { email: 1, _id: 1, name: 1 });
+  const { email } = user;
+
+  // eslint-disable-next-line max-len
+  let collectionName: any; // the name of the collection (intrviewee or interviewer)
+
+  // // query about role by _id
+  if (userInfo){
+    if (userInfo.role === 'interviewee') {
+      collectionName = Interviewee;
+    } else if (userInfo.role === 'interviewer') {
+      collectionName = Interviewer;
+    }
+  }
+  const result = await collectionName.updateOne({ 'interviews._id': interviewId }, { $set: { 'interviews.$.is_cancalled': true } });
+
+  // mail sender to Interviewee or Interviewer
+  const interview = await collectionName.findOne({ 'interviews._id': interviewId }, { interviews: 1, _id: 0 });
 
   if (!interview) {
     return res.json({
@@ -17,9 +37,11 @@ const cancalInterview = async (req: RequestType, res: Response) => {
 
   // deruct the interview object to got the interviews array
   const { interviews } = interview;
-  const interviewObj = interviews[0];
+  const { date, specilaization } = interviews[0];
 
-  const result = await Interviewee.updateOne({ 'interviews._id': interviewId }, { $set: { 'interviews.$.is_cancalled': !interviewObj.is_cancalled } });
+  await mailSender(email, 'Cancel Interview', `<h1> Cancel Interview </h1>
+    <h2>We inform you that your interview, scheduled for ${date}, which was an ${specilaization} specilaization, has been cancelled </h2>
+    <br> <h4>Thank you for your cooperation</h4>`);
 
   return res.json({ data: result, message: 'Interviews updated successfully' });
 };
--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { cancalInterview } from '../controllers';
+import { userAuth } from '../middlewares/auth';
 
 const router: Router = Router();
 
-router.patch('/:interviewId', cancalInterview);
+router.patch('/:interviewId', userAuth, cancalInterview);
 
 export default router;
