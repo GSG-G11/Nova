9bcbb46528fa499e040f02f1dd13f73b6a05c53f
ManarAbed <manarabed333@gmail.com>
2022-05-13 18:01:06 +0300

    handel save and filter on reviews card relates #59

--- client/src/components/Review/review.jsx
+++ client/src/components/Review/review.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-  Comment, Avatar, Tooltip, message,
+  Comment, Avatar, Tooltip, message, Select,
 } from 'antd';
 import {
   StarOutlined, StarFilled,
@@ -9,8 +9,10 @@ import {
 import './review.css';
 
 const ReviewCard = () => {
+  const { Option } = Select;
   const [reviews, setReviews] = useState([]);
   const [saved, setSaved] = useState(false);
+  const [filterVal, setFilterVal] = useState('all');
 
   useEffect(() => {
     (async () => {
@@ -26,31 +28,50 @@ const ReviewCard = () => {
     })();
   }, [reviews]);
 
+  const handleSave = async () => {
+    try {
+      const { message: savedMsg } = await axios.patch('/api/user/interview/review/:interviewId');
+      if (savedMsg === 'Successfully updated!') {
+        setSaved(!saved);
+      }
+    } catch ({ Response: { data: { message: msg } } }) {
+      message.error(msg);
+    }
+  };
+
   const actions = [
     <Tooltip key="comment-basic-like" title="Save">
-      <span>
-        { saved === false ? <StarOutlined /> : <StarFilled />}
-      </span>
+      <button type="button" onClick={handleSave}>
+        {saved ? <StarFilled /> : <StarOutlined />}
+      </button>
     </Tooltip>,
   ];
   return (
-    <div className="review-card">
-      { reviews.length > 0 ? reviews.map((review) => (
-        <Comment
-          actions={actions}
-          author="Han Solo"
-          avatar={
-            <Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
-          }
-          content={(
-            <p>
-              {review.message}
-              .
-            </p>
-          )}
-        />
-      )) : <div className="alert alert-primary"> There is no reviews until now </div>}
-    </div>
+    <>
+      <div className="filter-holder">
+        <Select defaultValue="all" style={{ width: 120 }} value={filterVal} onChange={(value) => setFilterVal(value)}>
+          <Option value="all">all</Option>
+          <Option value="saved">saved</Option>
+        </Select>
+      </div>
+      <div className="review-card">
+        {reviews.length > 0 ? reviews.map((review) => (
+          <Comment
+            actions={actions}
+            author="Han Solo"
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+            content={(
+              <p>
+                {review.message}
+                .
+              </p>
+            )}
+          />
+        )) : <div className="alert alert-primary"> There is no reviews until now </div>}
+        ,
+      </div>
+
+    </>
   );
 };
 export default ReviewCard;
