e07e5b8293d6a94c405ecb8f7e0e29b713fc3ef0
abedshamia <ab.shamia96@gmail.com>
2022-05-13 18:44:45 +0300

    Add Static Third Step

--- client/src/components/Forms/Interview/Steps/StepThree.jsx
+++ client/src/components/Forms/Interview/Steps/StepThree.jsx
@@ -1,9 +1,57 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { Input, Radio } from 'antd';
+
+const { Group } = Radio;
 // eslint-disable-next-line react/prop-types
 const StepThree = ({ title }) => (
   <div>
     <div className="interview-header">
       <p>{title}</p>
+    </div>
+    <div className="interview-step-content">
+      <Group buttonStyle="outline" size="large" className="interview-form__radio-group-third">
+        <Input
+          name="questionCategory"
+          type="radio"
+          id="Technical"
+          value="Technical"
+          className="interview-form__radio-input"
+        />
+        <label htmlFor="Technical">
+          Technical
+        </label>
+        <Input
+          name="questionCategory"
+          type="radio"
+          value="Analytical"
+          id="Analytical"
+          className="interview-form__radio-input"
+        />
+        <label htmlFor="Analytical">
+          Analytical
+        </label>
+        <Input
+          name="questionCategory"
+          type="radio"
+          value="Algorithms"
+          id="Algorithms"
+          className="interview-form__radio-input"
+        />
+        <label htmlFor="Algorithms">
+          Algorithms
+        </label>
+        <Input
+          name="questionCategory"
+          type="radio"
+          value="System Design"
+          id="System Design"
+          className="interview-form__radio-input"
+        />
+        <label htmlFor="System Design">
+          System Design
+        </label>
+      </Group>
     </div>
 
   </div>
