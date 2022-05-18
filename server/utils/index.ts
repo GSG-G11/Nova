94c9325d0f6a8a196a0e17964df53e5284b5cc07
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-18 14:35:02 +0300

    sort validation in utils index

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,14 +1,18 @@
 import { verifyToken, signToken } from './jwt';
 import mailSender from './sendEmailVerification/sendEmail';
-import signupValidation from './validation/signupValidation';
-import { loginValidation, postAvailableTimeValidation } from './validation';
+import {
+  loginValidation,
+  postAvailableTimeValidation,
+  getInterviewTimeValidation,
+  interviewValidation,
+  validateQuery,
+  getInterviewsQueryValidation,
+  signupValidation,
+  updateInfoValidation,
+} from './validation';
 import CustomError from './CustomError';
-import updateInfoValidation from './validation/updateInfoValidation';
 import { RequestType, signupInterface, updateInfoInterface } from './interfaces';
-import { validateQuery, getInterviewsQueryValidation } from './validation/queryValidation';
-import interviewValidation from './validation/interviewValidation';
 import emailTemplate from './email/interviewEmailTemplate';
-import getInterviewTimeValidation from './validation/getInterviewTimeValidation';
 
 export {
   getInterviewsQueryValidation,
