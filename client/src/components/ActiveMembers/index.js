ef41bf8ac8462afc67c4875e3b53305c3a267dfd
abedshamia <ab.shamia96@gmail.com>
2022-05-18 18:27:11 +0300

    Add Active Members Component

--- client/src/components/ActiveMembers/index.js
+++ client/src/components/ActiveMembers/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-// import { Typography } from 'antd';
+import { Typography } from 'antd';
 import SectionIntro from '../common/SectionIntro';
+import circle from '../../assets/images/circle.png';
+import person from '../../assets/images/person.png';
+import './style.css';
 
-// const { Title, Text } = Typography;
+const { Text } = Typography;
 const ActiveMembers = () => (
   <section className="active-members">
     <SectionIntro
@@ -11,6 +14,69 @@ const ActiveMembers = () => (
       description="Start training on job interviews with professional interviewers
       specialized in all fields of programming"
     />
+    <div className="active-members__members">
+
+      <div className="active-members__member-card">
+        <img
+          className="active-members__circle"
+          alt="circle"
+          src={circle}
+        />
+        <div className="active-members__member-image">
+          <img
+            src={person}
+            alt="member"
+          />
+        </div>
+        <Text strong className="active-members__member-name"> Fahim Rahman </Text>
+        <Text className="active-members__member-position"> Full Stack Developer </Text>
+      </div>
+      <div className="active-members__member-card">
+        <img
+          className="active-members__circle"
+          alt="circle"
+          src={circle}
+        />
+        <div className="active-members__member-image">
+          <img
+            src={person}
+            alt="member"
+          />
+        </div>
+        <Text strong className="active-members__member-name"> Fahim Rahman </Text>
+        <Text className="active-members__member-position"> Full Stack Developer </Text>
+      </div>
+      <div className="active-members__member-card">
+        <img
+          className="active-members__circle"
+          alt="circle"
+          src={circle}
+        />
+        <div className="active-members__member-image">
+          <img
+            src={person}
+            alt="member"
+          />
+        </div>
+        <Text strong className="active-members__member-name"> Fahim Rahman </Text>
+        <Text className="active-members__member-position"> Full Stack Developer </Text>
+      </div>
+      <div className="active-members__member-card">
+        <img
+          className="active-members__circle"
+          alt="circle"
+          src={circle}
+        />
+        <div className="active-members__member-image">
+          <img
+            src={person}
+            alt="member"
+          />
+        </div>
+        <Text strong className="active-members__member-name"> Fahim Rahman </Text>
+        <Text className="active-members__member-position"> Full Stack Developer </Text>
+      </div>
+    </div>
   </section>
 );
 
