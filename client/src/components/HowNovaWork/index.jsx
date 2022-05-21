8efc22a0d7111a446f77fa7a47da40c452172539
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-21 18:51:32 +0300

    reduce code Relates #81

--- client/src/components/HowNovaWork/index.jsx
+++ client/src/components/HowNovaWork/index.jsx
@@ -8,39 +8,40 @@ import actIt from '../../assets/images/pexels-pixabay-163064.png';
 const { Content } = Layout;
 const { Title } = Typography;
 
-const HowNovaWork = () => (
-  <Layout>
-    <Content
-      className="site-layout-background HowNovaWork"
-    >
-      <Title className="titleForHowNovaPractice" level={5}>How Nova Practicing Works</Title>
-      <div className="allImg">
-        <div className="getPairedDivText">
-          <img className="getPaired" src={getPaired} alt="getPaired" />
-          <div className="titleHrDiv">
-            <hr className="new1" />
-            <Title className="titleForAll" level={2}>Get Paired</Title>
-          </div>
-          <Title className="desc" level={5}>Tell us when and what you want to pracice and we’ll pair you with an optimal peer.</Title>
+const HowNovaWork = () => {
+  const arrImg = [{
+    img: getPaired,
+    title: 'Get Paired',
+    desc: 'Tell us when and what you want to practice and we’ll pair you with an optimal peer.',
+  }, {
+    img: practice,
+    title: 'Practice',
+    desc: 'Interviews are conducted using a collaborative environment over video. You and your peer will interview each other for 30-40 min.',
+  }, {
+    img: actIt,
+    title: 'Act It',
+    desc: 'Learn from peers’ feedback, again confidence and master the art of interviewing. keep practicing until you interview like a rock star.',
+  }];
+  return (
+    <Layout>
+      <Content
+        className="site-layout-background HowNovaWork"
+      >
+        <Title className="titleForHowNovaPractice" level={5}>How Nova Practicing Works</Title>
+        <div className="allImg">
+          {arrImg.map((item) => (
+            <div className="getPairedDivText" key={item.title}>
+              <img className="getPaired" src={item.img} alt={item.title} />
+              <div className="titleHrDiv">
+                <hr className="new1" />
+                <Title className="titleForAll" level={2}>{item.title}</Title>
+              </div>
+              <Title className="desc" level={5}>{item.desc}</Title>
+            </div>
+          ))}
         </div>
-        <div className="getPairedDivText">
-          <img className="getPaired" src={practice} alt="practice" />
-          <div className="titleHrDiv">
-            <hr className="new1" />
-            <Title className="titleForAll" level={2}>Practice</Title>
-          </div>
-          <Title className="desc" level={5}>Interviews are conducted using a collaborative environment over video. You and your peer will interview each other for 30-40 min.</Title>
-        </div>
-        <div className="getPairedDivText">
-          <img className="getPaired" src={actIt} alt="actIt" />
-          <div className="titleHrDiv">
-            <hr className="new1" />
-            <Title className="titleForAll" level={2}>Act it</Title>
-          </div>
-          <Title className="desc" level={5}>Learn from peers’ feedback, again confidence and master the art of interviewing. keep practicing until you interview like a rock star.</Title>
-        </div>
-      </div>
-    </Content>
-  </Layout>
-);
+      </Content>
+    </Layout>
+  );
+};
 export default HowNovaWork;
