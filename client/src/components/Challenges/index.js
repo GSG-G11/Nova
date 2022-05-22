696e53273bbf03e14c66f949dc946a34351d9304
abedshamia <ab.shamia96@gmail.com>
2022-05-22 11:57:59 +0300

    Add Challenge Component

--- client/src/components/Challenges/index.js
+++ client/src/components/Challenges/index.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import { Typography, Tag } from 'antd';
+import { Typography } from 'antd';
 import './style.css';
+import Challenge from './Challenge';
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
+
+const challengesArray = [
+  {
+    id: 1,
+    title: 'Sorted Lists',
+    name: 'You are given the heads of two sorted linked lists list1 and list2. Merge the two lists in a one sorted list.',
+    tag: 'easy',
+    link: 'https://leetcode.com/problems/merge-two-sorted-lists/',
+  },
+  {
+    id: 2,
+    title: 'Palindromic Substrings',
+    name: 'Given a string, return the number of palindromic substrings in it.',
+    tag: 'medium',
+    link: 'https://leetcode.com/problems/palindromic-substrings/',
+  },
+  {
+    id: 3,
+    title: 'Longest Valid Parentheses',
+    name: 'Given a string containing just the characters \'(\' and \')\', find the length of the longest valid parentheses substring.',
+    tag: 'hard',
+    link: 'https://leetcode.com/problems/longest-valid-parentheses/',
+  },
+];
 const Challenges = () => (
   <section className="top-challenges">
     <Title level={3} className="top-challenges__title">Top Challenges</Title>
     <div className="top-challenges__cards">
-      <div className="top-challenges__card">
-        <Text strong className="top-challenges__card-title">Challenge Title</Text>
-        <Text strong className="top-challenges__card-name">Challenge name</Text>
-        <Tag color="green" className="top-challenges__card-tag">
-          <Text strong>easy</Text>
-        </Tag>
-
-      </div>
-      <div className="top-challenges__card">
-        <Text strong className="top-challenges__card-title">Challenge Title</Text>
-        <Text strong className="top-challenges__card-name">Challenge name</Text>
-        <Tag color="green" className="top-challenges__card-tag">
-          <Text strong>easy</Text>
-        </Tag>
-
-      </div>
-      <div className="top-challenges__card">
-        <Text strong className="top-challenges__card-title">Challenge Title</Text>
-        <Text strong className="top-challenges__card-name">Challenge name</Text>
-        <Tag color="green" className="top-challenges__card-tag">
-          <Text strong>easy</Text>
-        </Tag>
-
-      </div>
+      {challengesArray.map(({
+        id, name, title, link, tag,
+      }) => (
+        <Challenge
+          key={id}
+          title={title}
+          name={name}
+          tag={tag}
+          link={link}
+        />
+      ))}
     </div>
   </section>
 );
