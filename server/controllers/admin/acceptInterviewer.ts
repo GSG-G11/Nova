93627ecd4c42ba35cf233813e66aaab5f018a3b1
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-22 14:45:28 +0300

    update approvet in interviewers collection Relates #111

--- server/controllers/admin/acceptInterviewer.ts
+++ server/controllers/admin/acceptInterviewer.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from 'express';
 import { Types } from 'mongoose';
+import { ObjectId } from 'mongodb';
 import { CustomError, acceptInterviewerValidation } from '../../utils';
-// import Interviewer from '../../database/Models/Interviewer';
+import Interviewer from '../../database/Models/Interviewer';
 
 const acceptInterviewer = async (req: Request, res: Response) => {
   const { id } = req.params;
-  // const { status } = req.body;
+  const { status } = req.body;
+
   await acceptInterviewerValidation(req.body);
+
   const validId = Types.ObjectId.isValid(id);
   if (!validId) {
     throw new CustomError('Invalid interview id!', 400);
   }
+
+  await Interviewer.updateOne({ userId: new ObjectId(id) }, {
+    $set: {
+      approved: status,
+    },
+  });
+
   res.json({
-    message: 'Interviewer accepted successfully!',
+    message: 'Application updated successfully!',
   });
 };
 
