54512ab87bcc2851bb7881277296b6e7868d2a78
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-23 11:18:49 +0300

    delete condition array

--- server/controllers/admin/getInterviewersAdmin.ts
+++ server/controllers/admin/getInterviewersAdmin.ts
@@ -17,7 +17,8 @@ const getInterviewersAdmin = async (req: RequestType, res: Response) => {
   const skip = (Number(page) - 1) * pageLimit;
 
   const dataBaseInterview = (role === 'interviewer') ? Interviewer : Interviewee;
-  const condition = (role === 'interviewer') ? [{
+
+  const user = await dataBaseInterview.aggregate([{
     $group: {
       _id: '$_id',
       userId: { $first: '$userId' },
@@ -50,34 +51,7 @@ const getInterviewersAdmin = async (req: RequestType, res: Response) => {
       ],
       as: 'userInfo',
     },
-  }] : [{
-    $group: {
-      _id: '$_id',
-      userId: { $first: '$userId' },
-    },
-  },
-  {
-    $lookup: {
-      from: 'users',
-      localField: 'userId',
-      foreignField: '_id',
-      pipeline: [
-        {
-          $project: {
-            name: 1,
-            email: 1,
-            role: 1,
-            profile_picture: 1,
-            cv: 1,
-            level: 1,
-          },
-        },
-      ],
-      as: 'userInfo',
-    },
-  }];
-
-  const user = await dataBaseInterview.aggregate(condition).skip(skip).limit(pageLimit);
+  }]).skip(skip).limit(pageLimit);
 
   res.json({
     data: user,
