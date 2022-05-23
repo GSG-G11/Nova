eaa205ba12d248e0e3b501c1163cc0508c133348
abedshamia <ab.shamia96@gmail.com>
2022-05-23 21:49:31 +0300

    Delete User from Interviewer/Interviewee Collections/

--- server/controllers/users/deleteUser.ts
+++ server/controllers/users/deleteUser.ts
@@ -1,5 +1,7 @@
 import { Response } from 'express';
 import { Types } from 'mongoose';
+import Interviewee from '../../database/Models/Interviewee';
+import Interviewer from '../../database/Models/Interviewer';
 import User from '../../database/Models/User';
 import { CustomError, RequestType } from '../../utils';
 
@@ -16,6 +18,12 @@ const deleteUser = async (req: RequestType, res: Response) => {
 
   if (!user) {
     throw new CustomError('Can not delete this user', 400);
+  }
+
+  if (user.role === 'interviewer') {
+    await Interviewer.findOneAndDelete({ userId: user._id });
+  } else {
+    await Interviewee.findOneAndDelete({ userId: user._id });
   }
 
   res.json({
