b6edc7a65fc3b1f8b741b3a19331e676fbd086a6
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-05-24 21:43:18 +0300

    delete all duplicate files

--- server/routes/interviewerRoutes 2.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import postAvailableTime from '../controllers/interviewer/postAvailableTime';
-import { userAuth } from '../middlewares/auth';
-
-const router : Router = Router();
-
-router.post('/schedule', userAuth, postAvailableTime);
-
-export default router;
