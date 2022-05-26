93af49c8b35151d15335c74462521b287314499e
abedshamia <ab.shamia96@gmail.com>
2022-05-26 23:20:44 +0300

    Fix active members profile for guests

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -5,12 +5,14 @@ import { useSelector } from 'react-redux';
 import ProfileTabs from '../ProfileTabs';
 import UserInfo from '../UserInfo';
 import Footer from '../common/Footer';
+import '../ProfileTabs/style.css';
 
 const Profile = () => {
   const { id } = useParams();
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
-  const { user: { id: loggedInUserId } } = useSelector((state) => state.auth);
+  const { user: loggedInUser } = useSelector((state) => state.auth);
+  const loggedInUserId = loggedInUser?.id;
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
@@ -38,11 +40,13 @@ const Profile = () => {
   }, [id]);
 
   return (
-    <section className="profile-container">
-      <UserInfo user={user} loading={loading} error={error} success={success} />
-      { loggedInUserId === id && <ProfileTabs user={user} /> }
+    <>
+      <section className="profile-container">
+        <UserInfo user={user} loading={loading} error={error} success={success} />
+        { loggedInUserId ? loggedInUserId === id && <ProfileTabs user={user} /> : null }
+      </section>
       <Footer />
-    </section>
+    </>
   );
 };
 
