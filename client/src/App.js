84ea3b47a0f63e6b94d55009589e7f2a5336d660
abedshamia <ab.shamia96@gmail.com>
2022-05-26 11:09:13 +0300

    Add Styling to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import 'antd/dist/antd.min.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import { Typography } from 'antd';
 import { checkUser } from './redux/features/auth/authSlice';
 import UserInfo from './components/UserInfo';
 import ProfileTabs from './components/ProfileTabs';
@@ -10,28 +11,45 @@ import Landing from './components/Landing';
 import VerifyAccount from './components/VerifyAccount';
 import AdminDashboard from './components/AdminDashboard';
 import NotFound from './components/NotFound';
+import LoadingSpinner from './components/common/LoadingSpinner';
 
 const App = () => {
   const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.auth);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
   useEffect(() => {
+    window.addEventListener('load', handleLoading);
     dispatch(checkUser());
+
+    return () => {
+      window.removeEventListener('load', handleLoading);
+    };
   }, []);
 
   return (
     <div className="App">
-      {loading ? 'Loading...' : (
-        <div>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/users/:id" element={<UserInfo />} />
-            <Route path="/auth/verify" element={<VerifyAccount />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <ProfileTabs />
+      {isLoading ? (
+        <div className="loading-home">
+          <Typography.Title level={4}>
+            Loading ...
+          </Typography.Title>
+          <LoadingSpinner />
         </div>
-      )}
+      )
+        : (
+          <div>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/users/:id" element={<UserInfo />} />
+              <Route path="/auth/verify" element={<VerifyAccount />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <ProfileTabs />
+          </div>
+        )}
 
     </div>
   );
