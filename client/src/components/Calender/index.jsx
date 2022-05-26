78833c63697be5e5f26139a6b1709cd9ed4e7500
Raghad-Mezied <rmezied3@gmail.com>
2022-05-26 14:08:25 +0300

    fix calender

--- client/src/components/Calender/index.jsx
+++ client/src/components/Calender/index.jsx
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import axios from 'axios';
 import {
-  DatePicker, Button, Form, message, Calendar, Badge,
+  DatePicker, Form, message, Calendar, Badge,
 } from 'antd';
 import './style.css';
 
 const { Item } = Form;
 
 const CalenderTab = () => {
-  const [dateTime, setDateTime] = useState('');
   const [availableDates, setAvailableDates] = useState([]);
 
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const getAvailableDate = async () => {
+      try {
+        const { data: { data } } = await axios.get('/api/interview/available', {
+          cancelToken: source.token,
+        });
+        setAvailableDates(data);
+      } catch ({ response: { data: { message: msg } } }) {
+        message.error(msg);
+      }
+    };
+    getAvailableDate();
+
+    return () => {
+      source.cancel();
+    };
+  }, [availableDates]);
+
   const onOk = (value) => {
-    setDateTime(value.format('YYYY-MM-DD HH'));
-  };
-  const postDate = async () => {
+    const dateTime = value.format('YYYY-MM-DD HH');
     const date = dateTime.split(' ')[0].toString();
     const time = Number(dateTime.split(' ')[1]);
     try {
@@ -28,9 +44,6 @@ const CalenderTab = () => {
       message.error({ msg });
     }
   };
-  const disabledDates = availableDates.length ? availableDates.map(({ date }) => date.split('T')[0]) : [];
-  const disabledDate = (current) => disabledDates.find((date) => date === moment(current).format('YYYY-MM-DD'));
-  // const dayArr = disabledDates.map((date) => parseInt(date.split('-')[2], 10));
 
   const dateFormat = (date) => (date.length === 1 ? `0${date}` : date);
 
@@ -60,51 +73,26 @@ const CalenderTab = () => {
     );
   };
 
-  useEffect(() => {
-    const source = axios.CancelToken.source();
-    const getAvailableDate = async () => {
-      try {
-        const { data: { data } } = await axios.get('/api/interview/available', {
-          cancelToken: source.token,
-        });
-        setAvailableDates(data);
-      } catch ({ response: { data: { message: msg } } }) {
-        message.error(msg);
-      }
-    };
-    getAvailableDate();
-
-    return () => {
-      source.cancel();
-    };
-  }, [dateTime]);
-
   const disabledDay = (current) => current && current < moment().endOf('day');
+
   return (
     <div className="calender-tab">
       <div className="timePicker">
         <Form layout="vertical" autoComplete="off" className="datepickerForm">
           <Item name="name" label="Select your available time" type="url">
             <DatePicker
-              value={dateTime}
               format="YYYY-MM-DD HH"
               showTime={{ defaultValue: moment('00:00:00', 'HH:mm:ss') }}
               disabledDate={disabledDay}
               onOk={onOk}
             />
           </Item>
-          <div className="btn-save-holder">
-            <Button type="primary" htmlType="submit" onClick={postDate}>
-              Save
-            </Button>
-          </div>
         </Form>
       </div>
       <div className="dateTimeCalender-holder">
         <h3 className="dates-sec">Your Available Dates</h3>
         <div className="calenders-holder">
           <Calendar
-            disabledDate={disabledDate}
             dateCellRender={dateCellRender}
           />
         </div>
