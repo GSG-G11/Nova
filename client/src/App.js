19186f86dacbc4d2390f2af966436d37932fbb70
abedshamia <ab.shamia96@gmail.com>
2022-05-26 11:38:27 +0300

    remove useEffect and add LoadingHome Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,40 +2,27 @@ import React, { useEffect } from 'react';
 import 'antd/dist/antd.min.css';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { Typography } from 'antd';
 import { checkUser } from './redux/features/auth/authSlice';
 import UserInfo from './components/UserInfo';
-
 import Landing from './components/Landing';
 import VerifyAccount from './components/VerifyAccount';
 import AdminDashboard from './components/AdminDashboard';
 import NotFound from './components/NotFound';
-import LoadingSpinner from './components/common/LoadingSpinner';
+import LoadingHome from './components/LoadingHome';
 
 const App = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = React.useState(true);
-  const handleLoading = () => {
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    window.addEventListener('load', handleLoading);
-    dispatch(checkUser());
 
-    return () => {
-      window.removeEventListener('load', handleLoading);
-    };
+  useEffect(() => {
+    dispatch(checkUser());
+    setIsLoading(false);
   }, []);
 
   return (
     <div className="App">
       {isLoading ? (
-        <div className="loading-home">
-          <Typography.Title level={4}>
-            Loading ...
-          </Typography.Title>
-          <LoadingSpinner />
-        </div>
+        <LoadingHome />
       )
         : (
           <div>
