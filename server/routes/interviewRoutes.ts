47ee05da97382e8e0bca051e422124060677bdd1
Raghad-Mezied <rmezied3@gmail.com>
2022-05-27 15:38:00 +0300

    remove create interview controller

--- server/routes/interviewRoutes.ts
+++ server/routes/interviewRoutes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import {
-  createInterview, getInterviewerAvailableTime,
+  getInterviewerAvailableTime,
   createReview, getAvailableTime,
 } from '../controllers';
 import { userAuth, checkInterviewer } from '../middlewares/auth';
 
 const router : Router = Router();
 
-// router.post('/interview', userAuth, createInterview);
 router.get('/interview/available', userAuth, getInterviewerAvailableTime);
 router.post('/interview/available', userAuth, getAvailableTime);
 router.post('/user/review/:id', userAuth, checkInterviewer, createReview);
